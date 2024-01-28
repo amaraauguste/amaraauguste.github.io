746d8009b77c8c46909dc104e87ca1c7ff6fe1f4
Amara <54652581+amaraauguste@users.noreply.github.com>
2024-01-28 12:52:02 -0500

    Create Rectangle.java

---
+++ courses/CISC1115/classwork_code/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle{
+     public static void main(String []args){
+         int length = 22;
+         int width = 18;
+         int area = length * width; //(l * w)
+         int perimeter = 2 * (length + width); //2(l + w)
+         System.out.println("Rectangle: length = " + length + " and width = " + width);
+         System.out.println("Area = " + area);
+         System.out.println("Perimeter = " + perimeter);
+     }
+}
