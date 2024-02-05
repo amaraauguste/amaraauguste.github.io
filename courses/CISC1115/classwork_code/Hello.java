b6f2c2c878ba49f571f19a6642ca1846a6f4c1fd
Amara <54652581+amaraauguste@users.noreply.github.com>
2024-02-04 21:42:18 -0500

    Delete courses/CISC1115/classwork_code/Hello.java

--- courses/CISC1115/classwork_code/Hello.java
+++
@@ -1,6 +0,0 @@
-public class Hello{
-     public static void main(String []args){
-          System.out.println("Hello, my name is Amara Auguste.");
-          System.out.println("How are you?");
-     }
-}
